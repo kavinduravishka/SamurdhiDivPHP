7f8f395a0074797376bfaaf8fbc1b4d108903aa0
kavinduravishaka <kavinduravishka98sl@gmail.com>
2020-05-06 08:23:41 +0530

    updated officer2 files

--- PHP/classes/query/officer2.query.php
+++ PHP/classes/query/officer2.query.php
@@ -45,7 +45,41 @@ class Offcer2query extends Dbh {
 	}
 
 	public function getMFRDetailes(){
-		
+
+		$arrBig = array();
+
+		$arrYatigaha = array();
+		$arrBadalgama = array();
+		$arrKongodamulla = array();
+		$arrKatuwellegama = array();
+		$arrWelangana = array();
+		$arrDunagaha = array();
+		$arrKotadeniyawa = array();
+		$arrDivulapitiya = array();
+		$arrWalpita = array();
+
+		$bank = '';
+
+		$sql = "SELECT COUNT(*) FROM detailsofbenificiaries WHERE Relief_Amount = ? and Bank_Zonal = ? and is_deleted = 0";
+
+		$stmt = $this->connect()->prepare($sql);
+
+		$bank = 'Yatigaha';
+
+		//fill later
+
+		array_push($arrBig,
+				"Yatigaha"=>$arrYatigaha,
+				"Badalgama"=>$arrBadalgama,
+				"Kongodamulla"=>$arrKongodamulla,
+				"Katuwellegama"=>$arrKatuwellegama,
+				"Welangana"=>$arrWelangana,
+				"Dunagaha"=>$arrDunagaha,
+				"Kotadeniyawa"=>$arrKotadeniyawa,
+				"Divulapitiya"=>$arrDivulapitiya,
+				"Walpita"=>$arrWalpita);
+
+
 	}
 	
 	
