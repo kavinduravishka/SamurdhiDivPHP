1523939cebe872f33512de1a9b8a6758cadb7a69
kavinduravishaka <kavinduravishka98sl@gmail.com>
2020-05-13 19:40:05 +0530

    just little changes

--- index/home-officer1.index.php
+++ index/home-officer1.index.php
@@ -1,5 +1,5 @@
 <?php session_start(); ?> 
-<?php require_once(realpath($_SERVER["DOCUMENT_ROOT"])."\SamurdhiDivPHP\classes\Officer1.class.php") ?>
+<?php require_once("../classes/Officer1.class.php") ?>
 
 
 <?php 
@@ -11,15 +11,18 @@
 <html>
 <head>
 	<title>Users</title>
-	<link rel="stylesheet" type="text/css" href="/SamurdhiDivPHP/css/main.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
 	<link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
     />
 </head>
 <body>
-	 <?php require_once(realpath($_SERVER["DOCUMENT_ROOT"])."\SamurdhiDivPHP\include\header.inc.php"); ?>
-	 <?php require_once(realpath($_SERVER["DOCUMENT_ROOT"])."\SamurdhiDivPHP\include\sidebar.inc.php"); ?>
+	 <?php //require_once(realpath($_SERVER["DOCUMENT_ROOT"])."\SamurdhiDivPHP\include\header.inc.php"); ?>
+	 <?php //require_once(realpath($_SERVER["DOCUMENT_ROOT"])."\SamurdhiDivPHP\include\sidebar.inc.php"); ?>
+
+	 <?php require_once("../include/header.inc.php"); ?>
+	 <?php require_once("../include/sidebar.inc.php"); ?>
 	
 	
 	<main >
