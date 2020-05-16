b3e220bd02f5fa06b2502a89aee940350240b8d5
DulajKavinda98 <kavindadulaj.1998@gmail.com>
2020-05-16 12:20:42 +0530

    officer4 changes/ officer2,3 changes

--- classes/officer4.class.php
+++ classes/officer4.class.php
@@ -24,4 +24,30 @@ class Officer4 extends Dbh{
         return $personalfile_list;        
     }
 
+
+    public function searchEntry($search){
+        $personalfile_list='';
+        $sql="SELECT * FROM personalfile WHERE (nic_number LIKE '%{$search}%' OR name LIKE '%{$search}%' ) AND is_deleted=0 ORDER BY date_appointed";
+        $stmt=$this->connect()->query($sql); 
+        $members=$stmt->fetchAll();
+
+        foreach($members as $member){           
+          
+            $personalfile_list.="<tr>";
+            $personalfile_list.="<td>{$member['nic_number']}</td>";
+            $personalfile_list.="<td>{$member['name']}</td>";
+            $personalfile_list.="<td>{$member['designation']}</td>";
+            $personalfile_list.="<td>{$member['service']}</td>";
+            $personalfile_list.="<td>{$member['date_appointed']}</td>";
+            $personalfile_list.="<td>{$member['date_of_birth']}</td>";
+            $personalfile_list.="<td>{$member['date_of_pension']}</td>";
+            $personalfile_list.="<td>{$member['w_op_number']}</td>";
+            $personalfile_list.="<td><a href=\"modify-member.index.php?member_id={$member['member_id']}\">Edit</a></td>";
+            $personalfile_list.="<td><a href=\"delete-member.index.php?member_id={$member['member_id']}\" onclick=\"return confirm('Are you sure?');\">Delete</a></td>";
+            $personalfile_list.="</tr>";
+        }
+        return $personalfile_list;        
+    }
+    
+
 }
