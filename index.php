1616ed39b94f6dc263fa927196b5cf323f725ed8
DulajKavinda98 <kavindadulaj.1998@gmail.com>
2020-07-19 19:35:11 +0530

    fbehrb

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-session_start();
+//session_start();
 
-include_once(realpath($_SERVER["DOCUMENT_ROOT"])."/SamurdhiDivPHP/view/loginView.php");
+require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/SamurdhiDivPHP/view/loginView.php");
