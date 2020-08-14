1f2742b1727110e765c71130f13de88d1352bc36
Kavindu <kavinduravishka98sl@gmail.com>
2020-08-15 00:28:07 +0530

    commit

--- view/includes/header.inc.php
+++ view/includes/header.inc.php
@@ -11,5 +11,3 @@
 		<div class=\"appname\">Samurdhi Division</div>
 		<div class=\"loggedin\">Welcome"?> <?php echo $_SESSION['user_name']; ?> <?php echo "! <a href=\"http://localhost/SamurdhiDivPHP/view/loginView.php\" class=\"logout_btn\">Log Out</a></div>
 	</header>"; ?>
-
-
