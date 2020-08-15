60f32fddde04ffbac578ffd34e1758b93432cd50
DulajKavinda98 <kavindadulaj.1998@gmail.com>
2020-08-15 22:13:18 +0530

    wfbwehd

--- view/includes/header.inc.php
+++ view/includes/header.inc.php
@@ -1,8 +1,5 @@
 <?php 
-	
-	echo "<input type=\"checkbox\" id=\"check\" />";
-
-
+	/* echo "<input type=\"checkbox\" id=\"check\" />";
 
 	echo "<header>
 		<label for=\"check\">
@@ -10,4 +7,36 @@
       </label>
 		<div class=\"appname\">Samurdhi Division</div>
 		<div class=\"loggedin\">Welcome"?> <?php echo $_SESSION['user_name']; ?> <?php echo "! <a href=\"http://localhost/SamurdhiDivPHP/view/loginView.php\" class=\"logout_btn\">Log Out</a></div>
-	</header>"; ?>
+	</header>";  */
+?>
+
+<div class="topnav">
+
+
+	<div id="header-begin"></div>
+
+	<div>
+		<a id="dashboard" class="active wsopt" href="#dashboard">Dashboard</a>
+  		<a id="form" class="wsopt" href="#forms">Forms</a>
+  		<a id="report" class="wsopt" href="#reports">Reports</a>
+	</div>
+	
+	<div>
+		<div style="width:20px ;height:50px ; float:right"></div>
+		<div class="userdiv" id="div_useroptions"> <a id="div_useroptbtn" class="useroptdrop" href="javascript:void(0)" onclick="dropmenushow();">&#9660</a> </div>
+		<div class="userdiv" id="div_username"><label>UserName</label></div>
+	</div>
+
+
+</div>
+ 
+
+<div id="dropmenu">
+		<div class="dropmenuopt"><a class="dropmenuoptlink" id="dmo_usrname" href="#">UserName</a></div>
+		<div class="dropmenuopt"><a class="dropmenuoptlink" id="dmo_profile" href="#">Profile</a></div>
+		<div class="dropmenuopt"><a class="dropmenuoptlink" id="dmo_files" href="#">Files</a></div>
+		<div class="dropmenuopt"><a class="dropmenuoptlink" id="logout" href="#">LogOut</a></div>
+</div>
+
+
+
---
+++ view/js/sidebar_js/kheader.js
@@ -0,0 +1,17 @@
+let optmenuclosed=1;
+
+function dropmenushow(){
+    if(optmenuclosed==1){
+        document.getElementById("dropmenu").style.visibility="visible";
+        document.getElementById("dropmenu").style.transform="scale(1)";
+        document.getElementById("dropmenu").style.msTransform="scale(1)";
+        document.getElementById("div_useroptbtn").innerHTML="&#9650";
+        optmenuclosed=0;
+    }else if(optmenuclosed==0){
+        document.getElementById("dropmenu").style.visibility="hidden";
+        document.getElementById("dropmenu").style.transform="scale(0)";
+        document.getElementById("dropmenu").style.msTransform="scale(1)";
+        document.getElementById("div_useroptbtn").innerHTML="&#9660";
+        optmenuclosed=1;
+    }
+}
