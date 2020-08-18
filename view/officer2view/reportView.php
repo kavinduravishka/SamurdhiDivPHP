78b13bb4da7e45c49e00aea2c451bb8f1a75939b
Kavindu <kavinduravishka98sl@gmail.com>
2020-08-18 07:11:29 +0530

    commit

---
+++ view/officer2view/reportView.php
@@ -0,0 +1,33 @@
+<?php  ?>
+
+<html>
+
+<!-- <meta name="pagedata" tab="task" type="form" title="Add member"> -->
+
+<head>
+	<link rel="stylesheet" type="text/css" href="../css/ksidebar.css" >
+	<link rel="stylesheet" type="text/css" href="../css/kheader.css" >
+	<link rel="stylesheet" type="text/css" href="../css/kbody.css" >
+    
+</head>
+
+<body>
+
+	
+
+<?php 
+include_once("../includes/ksidebar.inc.php");
+include_once("../includes/kheader.inc.php");
+?> 
+
+<div id="maincontainer">
+	<h1>Contents</h1>
+</div>
+
+
+	<script type="text/javascript" src="../js/ksidebar.js"></script>
+	<script type="text/javascript" src="../js/kheader.js"></script>
+	<script type="text/javascript" src="../js/konload.js"></script>
+</body>
+
+</html>
