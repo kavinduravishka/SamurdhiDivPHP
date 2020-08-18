d4e30457fc997bff834053ba1012dcfa033cc0b2
Kavindu <kavinduravishka98sl@gmail.com>
2020-08-18 06:11:19 +0530

    commit

--- view/includes/test_header_sidebar/konsubmit.js
+++ view/includes/test_header_sidebar/konsubmit.js
@@ -1,3 +1,25 @@
-function checkProvidedRequired{
-    var inputs = document.getElementsByTagName('input')
+function validate(){
+    var inputs = document.getElementsByClassName('forminput');
+
+    return checkRequired(inputs);
 }
+
+function checkRequired(inputs){
+    var input=null;
+    var span=null;
+
+    var passed=true;
+
+    for(var i =0;i<inputs.length;i++){
+        input=inputs[i];
+        if(input.required==true){
+            span = document.getElementById(input.getAttribute('spanid'));
+            if(input.value==""){
+                span.innerHTML="This field is required";
+                passed=false;
+            }
+        }
+    }
+    return passed;
+}
+
