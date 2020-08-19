778a22444ad214f5c93ff43437c4c85c5dd4cf39
Kavindu <kavinduravishka98sl@gmail.com>
2020-08-19 09:53:47 +0530

    c

--- contr/officer2Contr/officer2.class.php
+++ contr/officer2Contr/officer2.class.php
@@ -29,7 +29,7 @@ class Officer2 extends Officer{
     
     protected function handleLogin($login_request){
         if ($login_request=='officer2') {
-            header('Location: http://localhost/SamurdhiDivPHP/view/officer2view/officer2-dashboardView.php');
+            header('Location: http://localhost/SamurdhiDivPHP/view/officer2view/dashboardView.php');
             return TRUE;
         }
         else {
