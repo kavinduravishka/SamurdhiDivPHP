a8858061f40fa2b04348a87448045edc23a72095
Kavindu <kavinduravishka98sl@gmail.com>
2020-08-24 00:11:38 +0530

    c

--- view/includes/islogged.inc.php
+++ view/includes/islogged.inc.php
@@ -7,16 +7,18 @@ function checklogged($uid){
         
         $nuid=$_SESSION['user_id'];
 
-        if($nuid=="1"){
-            header('Location: /SamurdhiDivPHP/');
+        if($uid==$nuid){
+            return;
+        }else if($nuid=="1"){
+            header('Location: ');
         }else if($nuid=="2"){
             header('Location: /SamurdhiDivPHP/view/officer2view/dashboardView.php');
         }else if($nuid=="3"){
             header('Location: /SamurdhiDivPHP/view/officer3view/dashboardView.php');
         }else if($nuid=="4"){
-            header('Location: /SamurdhiDivPHP/');
+            header('Location: ');
         }else if($nuid=="5"){
-            header('Location: /SamurdhiDivPHP/');
+            header('Location: ');
         }
 
     }
