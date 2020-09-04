c4b87f0045d184643167bd863e25025330d8a70d
yazi99 <56828636+yazi99@users.noreply.github.com>
2020-09-04 14:09:29 +0530

    y

--- contr/officer1Contr/bcrKatuwelgama.class.php
+++ contr/officer1Contr/bcrKatuwelgama.class.php
@@ -37,8 +37,8 @@ public function printReport($data){
 
   public function viewReport(){
     
-      $gn_divisions = array('41-Paranahalpe','41A-Paranahalpe','42-Halpe','42A-Nariyamulla','42B-Gurullagama','43-Pahala-Kithulwala','43A-Ihala-Kithulwala','43B-Uthuru-Kithulwala','44-Waththemulla','44A-Ihala-Kithulgala','44B-Pahala-Kithulgala','45-Kuligedara','46-Pahala-Madithiyawala','46A-Pahala-Madithiyawala','47-Hangawaththa');
-      $gn_divisions_sinhala = array('41 බ/පරණහල්පේ','41A නැ/පරණහල්පේ','42 හල්පේ','42A නරියමුල්ල','42B ගුරුගල්ලගම','43 පහල කිතුල්වල','43A ඉහල කිතුල්වල','43B උතුරු කිතුල්වල','44 වත්තෙමුල්ල','44A ඉහල කලුඅග්ගල','44B පහල කලුඅග්ගල','45 කුලී ගෙදර','46 පහල මඩිතියවල','46A පහල මඩිතියවල','47 හඟවත්ත');
+      $gn_divisions = array('Kadawala North','Kadwala','Dagonna-North','Dagonna-East','Halgahawelawatta','Katuwelgama-North','Kadawala-South','Aluthepola-West','Thotillagahawatta','Katuwelgama-South','Palugahawela','Dagonna-South');
+      $gn_divisions_sinhala = array('කඩවල උතුර','කඩවල','දාගොන්න උතුර','දාගොන්න නැගෙනහිර','හල්ගහවෙලවත්ත','කටුවෙලේගම උතුර','කඩවල දකුණ','අලුත්පොල බටහිර','තොටිල්ලගහවත්ත','කටුවෙලේගම දකුණ',' පලුගහවෙල','ගොන්න දකුණ');
       
       $count_list = "";
       for($i=0; $i<15; $i++){
