eae868ea44adc84c1c4bf933bd9e941dbe335baf
yazi99 <56828636+yazi99@users.noreply.github.com>
2020-12-15 23:19:27 +0530

    y

--- contr/reportContr/reportfactory.class.php
+++ contr/reportContr/reportfactory.class.php
@@ -3,6 +3,8 @@
 require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/SamurdhiDivPHP/contr/reportContr/bcreportfactory.class.php");
 require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/SamurdhiDivPHP/contr/reportContr/ireport.php");
 
+
+//Factory class
 abstract class ReportFactory{
 
     public function anOperation($type):Report{
--- view/includes/officer1Inc/beneficiariesInc.php
+++ view/includes/officer1Inc/beneficiariesInc.php
@@ -1,23 +1,21 @@
-<?php session_start(); ?>
-<?php 
-        //require_once("../contr/officer4Contr/officer4.class.php");
-		require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/SamurdhiDivPHP/contr/officer1Contr/officer1.class.php");
-	// 	if($_SESSION['user_id']!=1){
-    //     header('Location: /SamurdhiDivPHP/view/loginView.php');
-    // }
-          
-?>
+<?php session_start(); 
 
-<?php
+	require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/SamurdhiDivPHP/contr/officer1Contr/officer1.class.php");        
+	
+	
+	
+	if($_SESSION['user_id']!=1){
+        header('Location: /SamurdhiDivPHP/view/loginView.php');
+    }
 
 	$search = '';
 
 	$officer1 = Officer1::getInstance();
-		if(isset($_GET['search'])){
-			$search = $_GET['search'];
-			$beneficiaryList = $officer1->searchBeneficiary($search);
-		}else{
-		    $beneficiaryList = $officer1->loadBeneficiaries();
-		}
+			if(isset($_GET['search'])){
+				$search = $_GET['search'];
+				$beneficiaryList = $officer1->searchBeneficiary($search);
+			}else{
+				$beneficiaryList = $officer1->loadBeneficiaries();
+			}
 
 ?>
