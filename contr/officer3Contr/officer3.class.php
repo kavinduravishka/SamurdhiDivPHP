78ad739289a50942c6041b7fd368e62443e7d64e
Kavindu <kavinduravishka98sl@gmail.com>
2020-12-16 18:32:09 +0530

    commit

--- contr/officer3Contr/officer3.class.php
+++ contr/officer3Contr/officer3.class.php
@@ -85,6 +85,39 @@ class Officer3 extends Officer{
             </tr>
 EOD;
         foreach ($fetched as $row) {
+
+            $period='';
+
+            switch ($row['PeriodID']){
+                case 1:
+                    $period="2018-2020";
+                    break;
+                case 2:
+                    $period="2019-2021";
+                    break;
+                case 3:
+                    $period="2020-2022";
+                    break;
+                case 4:
+                    $period="2021-2023";
+                    break;
+                case 5:
+                    $period="2022-2024";
+                    break;
+                case 6:
+                    $period="2023-2025";
+                    break;
+                case 7:
+                    $period="2024-2026";
+                    break;
+                case 8:
+                    $period="2025-2027";
+                    break;
+                case 9:
+                    $period="2026-2028";
+                    break;
+            }
+            
             $fetchlist.=<<<EDO
             <tr>
                 <td>{$row['Name']}</td>
@@ -98,7 +131,7 @@ EOD;
                 <td>{$row['Address']}</td>
                 <td>{$row['Bank']}</td>
                 <td>{$row['SisurakaAcNo']}</td>
-                <td>{$row['PeriodID']}</td>
+                <td>{$period}</td>
             </tr>
 EDO;
         }
